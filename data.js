70a25049c0dba30d15a40d2e5d5ec69bea939314
bulbmin191 <jolielimthetoast56@gmail.com>
2024-10-05 14:34:33 +0800

    First Commit
    First upload of FOE tour

---
+++ data.js
@@ -0,0 +1,163 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.43513926069994646,
+        "pitch": 0,
+        "fov": 1.5016331376983532
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2596399548844861,
+          "pitch": -0.18785889016197288,
+          "rotation": 0,
+          "target": "1-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7306253973612638,
+          "pitch": 0.029600378494013313,
+          "title": "u-turn",
+          "text": "no u"
+        }
+      ]
+    },
+    {
+      "id": "1-engineering-auditorium",
+      "name": "Engineering Auditorium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.29942083377796536,
+        "pitch": -0.17638811042574787,
+        "fov": 1.5016331376983532
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6445184000409796,
+          "pitch": 0.5788519229523317,
+          "rotation": 3.141592653589793,
+          "target": "0-along-engineering-drive-1"
+        },
+        {
+          "yaw": 0.0480167722413718,
+          "pitch": 0.12592451083251888,
+          "rotation": 6.283185307179586,
+          "target": "2-engineering-auditorium-atrium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.09552186386013517,
+          "pitch": -0.10211912976783566,
+          "title": "auditorium",
+          "text": "bottom text"
+        },
+        {
+          "yaw": -1.3379147679489805,
+          "pitch": 0.06475938224213174,
+          "title": "artpiece",
+          "text": "wow swirly"
+        }
+      ]
+    },
+    {
+      "id": "2-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.906260235773363,
+        "pitch": 0.0883132205754027,
+        "fov": 1.4197691413773283
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.064883009383456,
+          "pitch": 0.1909875854434535,
+          "rotation": 11.780972450961727,
+          "target": "1-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.496649770533459,
+          "pitch": 0.16833089679297508,
+          "title": "Spinelli Coffee",
+          "text": "it's gone :("
+        },
+        {
+          "yaw": -2.3652464065674597,
+          "pitch": -0.0028948219021636135,
+          "title": "mysterious door",
+          "text": "where does go"
+        }
+      ]
+    }
+  ],
+  "name": "FOE ahffhskhfksdhbfsdkjbfmbcds",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
